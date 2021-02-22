0c0784c39da6e446c036994f6062fbe66f5721ad
Dhoiney <dhoiney@mail.ru>
2021-02-22 21:45:45 +0300

    end of 1 django lesson

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post,Comment,Comment2
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Comment2)
